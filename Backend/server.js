2f4cb97b2d6efbf470e59bf7c7c4debc6d2303cb
rahulambhore21 <ambhorerahul69@gmail.com>
2025-03-18 13:50:51 +0530

    cors set

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(cors());
+app.use(cors("https://event-management-frontend-kqj6.onrender.com"));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
